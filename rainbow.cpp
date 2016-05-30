d47f7b4af40ee355bcaf905751a16bb4b54413d6
Nand-e <zsiraffoci@gmail.com>
2016-05-30 16:42:51 +0200

    First step
    Only fpr test

---
+++ rainbow.cpp
@@ -0,0 +1,162 @@
+
+#include <Arduino.h>
+extern Adafruit_ST7735 tft;
+
+// Meter colour schemes
+#define RED2RED 0
+#define GREEN2GREEN 1
+#define BLUE2BLUE 2
+#define BLUE2RED 3
+#define GREEN2RED 4
+#define RED2GREEN 5
+
+// Color definitions
+#define ILI9341_BLACK       0x0000      /*   0,   0,   0 */
+#define ILI9341_NAVY        0x000F      /*   0,   0, 128 */
+#define ILI9341_DARKGREEN   0x03E0      /*   0, 128,   0 */
+#define ILI9341_DARKCYAN    0x03EF      /*   0, 128, 128 */
+#define ILI9341_MAROON      0x7800      /* 128,   0,   0 */
+#define ILI9341_PURPLE      0x780F      /* 128,   0, 128 */
+#define ILI9341_OLIVE       0x7BE0      /* 128, 128,   0 */
+#define ILI9341_LIGHTGREY   0xC618      /* 192, 192, 192 */
+#define ILI9341_DARKGREY    0x7BEF      /* 128, 128, 128 */
+#define ILI9341_BLUE        0x001F      /*   0,   0, 255 */
+#define ILI9341_GREEN       0x07E0      /*   0, 255,   0 */
+#define ILI9341_CYAN        0x07FF      /*   0, 255, 255 */
+#define ILI9341_RED         0xF800      /* 255,   0,   0 */
+#define ILI9341_MAGENTA     0xF81F      /* 255,   0, 255 */
+#define ILI9341_YELLOW      0xFFE0      /* 255, 255,   0 */
+#define ILI9341_WHITE       0xFFFF      /* 255, 255, 255 */
+#define ILI9341_ORANGE      0xFD20      /* 255, 165,   0 */
+#define ILI9341_GREENYELLOW 0xAFE5      /* 173, 255,  47 */
+#define ILI9341_PINK        0xF81F
+
+
+
+
+// #########################################################################
+//  Draw the meter on the screen, returns x coord of righthand side
+// #########################################################################
+int ringMeter(int value, int vmin, int vmax, int x, int y, int r, char *units, byte scheme)
+{
+  // Minimum value of r is about 52 before value text intrudes on ring
+  // drawing the text first is an option
+  
+  x += r; y += r;   // Calculate coords of centre of ring
+  int  w = r / 4;    // Width of outer ring is 1/4 of radius  
+  int  angle = 150;  // Half the sweep angle of meter (300 degrees)
+  int  text_colour = 0; // To hold the text colour
+  int  v = map(value, vmin, vmax, -angle, angle); // Map the value to an angle v
+  byte seg = 5; // Segments are 5 degrees wide = 60 segments for 300 degrees
+  byte inc = 5; // Draw segments every 5 degrees, increase to 10 for segmented ring
+
+  // Draw colour blocks every inc degrees
+  for (int i = -angle; i < angle; i += inc) {
+
+    // Choose colour from scheme
+    int colour = 0;
+    switch (scheme) {
+      case 0: colour = ILI9341_RED; break; // Fixed colour
+      case 1: colour = ILI9341_GREEN; break; // Fixed colour
+      case 2: colour = ILI9341_BLUE; break; // Fixed colour
+      case 3: colour = rainbow(map(i, -angle, angle, 0, 127)); break; // Full spectrum blue to red
+      case 4: colour = rainbow(map(i, -angle, angle, 63, 127)); break; // Green to red (high temperature etc)
+      case 5: colour = rainbow(map(i, -angle, angle, 127, 63)); break; // Red to green (low battery etc)
+      default: colour = ILI9341_BLUE; break; // Fixed colour
+    }
+
+    // Calculate pair of coordinates for segment start
+    float sx = cos((i - 90) * 0.0174532925);
+    float sy = sin((i - 90) * 0.0174532925);
+    uint16_t x0 = sx * (r - w) + x;
+    uint16_t y0 = sy * (r - w) + y;
+    uint16_t x1 = sx * r + x;
+    uint16_t y1 = sy * r + y;
+
+    // Calculate pair of coordinates for segment end
+    float sx2 = cos((i + seg - 90) * 0.0174532925);
+    float sy2 = sin((i + seg - 90) * 0.0174532925);
+    int x2 = sx2 * (r - w) + x;
+    int y2 = sy2 * (r - w) + y;
+    int x3 = sx2 * r + x;
+    int y3 = sy2 * r + y;
+
+    if (i < v) { // Fill in coloured segments with 2 triangles
+      tft.fillTriangle(x0, y0, x1, y1, x2, y2, colour);
+      tft.fillTriangle(x1, y1, x2, y2, x3, y3, colour);
+      text_colour = colour; // Save the last colour drawn
+    }
+    else // Fill in blank segments
+    {
+      tft.fillTriangle(x0, y0, x1, y1, x2, y2, 0x0005);
+      tft.fillTriangle(x1, y1, x2, y2, x3, y3, 0x0005);
+    }
+  }
+
+  // Convert value to a string
+  char buf[10];
+  byte len = 4; if (value > 999) len = 5;
+  dtostrf(value, len, 0, buf);
+
+  // Set the text colour to default
+  tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
+  // Uncomment next line to set the text colour to the last segment value!
+  // tft.setTextColor(text_colour, ILI9341_BLACK);
+  
+  // Print value, if the meter is large then use big font 6, othewise use 4
+  //if (r > 84) tft.drawCentreString(buf, x - 5, y - 20, 6); // Value in middle
+ // else tft.drawCentreString(buf, x - 5, y - 20, 4); // Value in middle
+
+  // Print units, if the meter is large then use big font 4, othewise use 2
+  tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
+  //if (r > 84) tft.drawCentreString(units, x, y + 30, 4); // Units display
+ // else tft.drawCentreString(units, x, y + 5, 2); // Units display
+
+  // Calculate and return right hand side x coordinate
+  return x + r;
+}
+
+// #########################################################################
+// Return a 16 bit rainbow colour
+// #########################################################################
+unsigned int rainbow(byte value)
+{
+  // Value is expected to be in range 0-127
+  // The value is converted to a spectrum colour from 0 = blue through to 127 = red
+
+  byte red = 0; // Red is the top 5 bits of a 16 bit colour value
+  byte green = 0;// Green is the middle 6 bits
+  byte blue = 0; // Blue is the bottom 5 bits
+
+  byte quadrant = value / 32;
+
+  if (quadrant == 0) {
+    blue = 31;
+    green = 2 * (value % 32);
+    red = 0;
+  }
+  if (quadrant == 1) {
+    blue = 31 - (value % 32);
+    green = 63;
+    red = 0;
+  }
+  if (quadrant == 2) {
+    blue = 0;
+    green = 63;
+    red = value % 32;
+  }
+  if (quadrant == 3) {
+    blue = 0;
+    green = 63 - 2 * (value % 32);
+    red = 31;
+  }
+  return (blue << 11) + (green << 5) + red;
+}
+
+// #########################################################################
+// Return a value in range -1 to +1 for a given phase angle in degrees
+// #########################################################################
+float sineWave(int phase) {
+  return sin(phase * 0.0174532925);
+}
+ 
